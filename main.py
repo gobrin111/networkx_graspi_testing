291601baf8da5e0b4af676af7aee63b92897dc35
manla <mleung8@buffalo.edu>
2024-10-09 13:35:59 -0400

    #86 Updated implementation with red and blue vertices at the end

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import graspi_networkx as gn
+import argparse
+import networkx as nx
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description="Run Python code with user input")
+parser.add_argument("input_value", help="Input value to process")
+args = parser.parse_args()
+args = args.input_value
 
 def main():
     #Node 0 is Blue and 1 is Red
-    G = gn.createGraph("./2x2.txt")
+    #Graph Creation
+    G = gn.createGraph(args)
     print("Nodes:", G.nodes())
     print("Edges:", G.edges())
 
-    subgraph = gn.filterGraph(G)
-    print("Subgraph Nodes:", subgraph.nodes())
-    print("Subgraph Edges:", subgraph.edges())
+    #Graph Filtering
+    filteredGraph = gn.filterGraph(G)
+    print("Subgraph Nodes:", filteredGraph.nodes())
+    print("Subgraph Edges:", filteredGraph.edges())
+
+    #BFS
+    bfs = gn.bfs(filteredGraph)
+
+    #Visualization
+    nx.draw(G, with_labels=True)
+    plt.show()
 
 if __name__ == "__main__":
     main()
