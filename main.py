b94fd69814dd4445d51100e1de25e1e4d541266a
Qi Pan <143447782+gobrin111@users.noreply.github.com>
2024-10-12 17:02:26 -0400

    card 91, added testing information
    the csv maker function, and comments to help understand what it does

--- main.py
+++ main.py
@@ -3,10 +3,58 @@ import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
 
+import csv
+import tracemalloc
+import time
+
 parser = argparse.ArgumentParser(description="Run Python code with user input")
 parser.add_argument("input_value", help="Input value to process")
 args = parser.parse_args()
 args = args.input_value
+
+#obtains the average runtimes for each operation
+#and gets the memory usage for overall
+def writeRow(txt):
+    row = []
+    finalTotal = 0
+    total = 0
+    g = createGraph(txt)
+    for i in range(3):
+        start = time.time()
+        g=createGraph(txt)
+        total+=time.time()-start
+    total = total/3
+    finalTotal +=total
+    row.append(total)
+    total = 0
+
+    for i in range(3):
+        start = time.time()
+        g_filtered = filterGraph(g)
+        total+=time.time()-start
+    total = total / 3
+    finalTotal+=total
+    row.append(total)
+    total = 0
+
+    num_nodes = g.number_of_nodes()
+    for i in range(3):
+        start = time.time()
+        bfs_paths=nx.single_source_shortest_path(g_filtered, source=num_nodes-1)
+        total+=time.time()-start
+    total = total / 3
+    finalTotal += total
+    row.append(total)
+    row.append(finalTotal)
+    tracemalloc.start()
+    g=createGraph(txt)
+    g_filtered = filterGraph(g)
+    bfs_paths=nx.single_source_shortest_path(g_filtered, source=num_nodes-1)
+    stats = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    stats = stats[1] - stats[0]
+    row.append(stats)
+    return row
 
 def main():
     #Graph Creation
@@ -27,6 +75,21 @@ def main():
     #Visualization
     nx.draw(G, with_labels=True)
     plt.show()
+
+    #writes to the csv file all the calculated times and memory usage
+    # first column is the average runtime for graph creation
+    # second column is the average runtime for graph filtering
+    # third is the average runtime for BFS search
+    # fourth is the overall average runtime
+    # fifth is the overall memory usage for doing everything
+
+    # runtime is measured in seconds
+    # and memory is measured in bytes
+    # results are sent to the out.csv file in the same directory
+    row = writeRow(args)
+    with open('out.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
 
 if __name__ == "__main__":
     main()
